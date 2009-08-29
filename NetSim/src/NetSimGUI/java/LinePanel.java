cbc1a0f1cff900738de1bd3e849a7d14df2f84a0
alex.gessner <alex.gessner@72b2d484-9282-11de-a5e2-01e6cdb70936>
2009-08-29 19:33:10 +0000

    git-svn-id: https://cs6240networksimulator.googlecode.com/svn/trunk@32 72b2d484-9282-11de-a5e2-01e6cdb70936

---
+++ NetSim/src/NetSimGUI/java/LinePanel.java
@@ -0,0 +1,53 @@
+package NetSimGUI.java;
+
+import java.awt.Graphics;
+import java.awt.LayoutManager;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+
+public class LinePanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2812343318256065006L;
+	public Vector<Integer> linevector;
+
+	public LinePanel() {
+		// TODO Auto-generated constructor stub
+		linevector = new Vector<Integer>();
+	}
+
+	public LinePanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinePanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LinePanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void paint(Graphics g) {
+		int a,b,c,d;
+		
+        super.paint(g);
+        
+        Iterator<Integer> i = this.linevector.iterator();
+        while (i.hasNext()) {
+        	a = i.next();
+        	b = i.next();
+        	c = i.next();
+        	d = i.next();
+        	g.drawLine(a, b, c, d);
+        }
+        
+    } 
+
+}
